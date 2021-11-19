376393d3dd03f34a948ea73e6f76933e31bfbb33
unyuu <unyuu@tenshi.nekomimi.nya>
2021-11-19 17:23:41 +0500

    Вывод теперь красивый.
    Главное - выше main() не смотреть :)

--- 20211119-experiment-2/main.cc
+++ 20211119-experiment-2/main.cc
@@ -34,36 +34,62 @@ public:
 	double x {}, y {};
 
 	Coordinates() = default;
+	Coordinates(double x, double y): x(x), y(y) {}
 	virtual ~Coordinates() = default;
 };
+
+class Point: public Coordinates
+{
+public:
+	Point() = default;
+	Point(double x, double y): Coordinates(x, y) {}
+	virtual ~Point() = default;
+};
+
+class Vector: public Coordinates
+{
+public:
+	Vector() = default;
+	Vector(double x, double y): Coordinates(x, y) {}
+	virtual ~Vector() = default;
+};
+
+std::istream& operator >>(std::istream &in, Coordinates &c)
+{
+	in >> c.x >> c.y;
+	return in;
+}
+
+std::ostream& operator <<(std::ostream &out, const Coordinates& c)
+{
+	out << c.x << ',' << c.y;
+	return out;
+}
 
 int main()
 {
 	using std::cin;
 	using std::cout;
-	Coordinates p;
-	Coordinates v0;
-	Coordinates v1;
-	Coordinates v2;
-	Coordinates v3;
-
-	v0.x = 1;
-	v0.y = 1;
+	Point p;
+	Vector v0 {1, 1};
+	Vector v1;
+	Vector v2;
+	Vector v3;
 
 	cout << "Введите координаты точки p: ";
-	cin >> p.x >> p.y;
+	cin >> p;
 	cout << "Введите координаты вектора v1: ";
-	cin >> v1.x >> v1.y;
+	cin >> v1;
 	cout << "Введите координаты вектора v2: ";
-	cin >> v2.x >> v2.y;
+	cin >> v2;
 
-	v3.x = v1.x + v2.x;
-	v3.y = v1.y + v2.y;
-	cout << "v3 = {" << v3.x << ", " << v3.y << "}\n";
+	v3.x = v1.x + v2.x + v0.x;
+	v3.y = v1.y + v2.y + v0.x;
+	cout << "v3 = {" << v3 << "}\n";
 
 	p.x += v3.x;
 	p.y += v3.y;
-	cout << "p  = (" << p.x << ", " << p.y << ")\n";
+	cout << "p  = (" << p << ")\n";
 
 	return 0;
 }
