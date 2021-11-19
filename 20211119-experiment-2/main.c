882f4170ea49a993a4a77c8f249e8ef0dd7abf13
unyuu <unyuu@tenshi.nekomimi.nya>
2021-11-19 16:50:48 +0500

    Попытка сделать ввод-вывод красивым (неудачная)

--- 20211119-experiment-2/main.c
+++ 20211119-experiment-2/main.c
@@ -10,6 +10,16 @@ typedef struct {
 	double x, y;
 } coords_t;
 
+void read_coords(coords_t * where)
+{
+	scanf("%lf%lf", &where->x, &where->y);
+}
+
+void print_coords(const coords_t * what)
+{
+	printf("%.3lf,%.3lf", what->x, what->y);
+}
+
 int main()
 {
 	coords_t p;
@@ -18,20 +28,23 @@ int main()
 	coords_t v3;
 
 	printf("Введите координаты точки p: ");
-	scanf("%lf%lf", &p.x, &p.y);
+	read_coords(&p);
 	printf("Введите координаты вектора v1: ");
-	scanf("%lf%lf", &v1.x, &v1.y);
+	read_coords(&v1);
 	printf("Введите координаты вектора v2: ");
-	scanf("%lf%lf", &v2.x, &v2.y);
+	read_coords(&v2);
 
 	v3.x = v1.x + v2.x;
 	v3.y = v1.y + v2.y;
-	printf("v3 = {%.3lf,%.3lf}\n", v3.x, v3.y);
+	printf("v3 = {");
+	print_coords(&v3);
+	printf("}\n");
 
 	p.x += v3.x;
 	p.y += v3.y;
-	printf("p = (%.3lf,%.3lf)\n", p.x, p.y);
-
+	printf("p  = (");
+	print_coords(&p);
+	printf(")\n");
 
 	return 0;
 }
